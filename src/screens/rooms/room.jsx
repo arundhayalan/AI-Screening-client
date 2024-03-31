c9ea19999d54a53ca3c6d64d81bcb2abb2d6bc91
arundhayalan <madhavanarun17@gmail.com>
2024-03-31 08:55:45 +0530

    videouploaded work

--- src/screens/rooms/room.jsx
+++ src/screens/rooms/room.jsx
@@ -3,6 +3,8 @@ import './room.css';
 import Button from '../../components/button/button';
 import { IoRecording } from "react-icons/io5";
 import { FaDownload } from "react-icons/fa6";
+import { FaUpload } from "react-icons/fa";
+import axios from 'axios';
 
 
 const Room = () => {
@@ -59,6 +61,7 @@ const Room = () => {
   const downloadRecording = () => {
     const blob = new Blob(recordedChunks, { type: 'video/webm' });
     const url = URL.createObjectURL(blob);
+    console.log(url);
     const a = document.createElement('a');
     document.body.appendChild(a);
     a.style = 'display: none';
@@ -66,15 +69,44 @@ const Room = () => {
     a.download = 'recording.webm';
     a.click();
     window.URL.revokeObjectURL(url);
+    
   };
+
+  const uploadRecording = async () => {
+    const blob = new Blob(recordedChunks, { type: 'video/webm' }); // Create Blob from recorded chunks
+    console.log(blob);
+    const formData = new FormData();
+    const videoname = generateVideoname();
+    formData.append('recording', blob, videoname); // Append Blob with filename to FormData
+
+    try {
+      const response = await axios.post('http://localhost:5002/video/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      console.log('Recording uploaded successfully:', response.data);
+    } catch (error) {
+      console.error('Error uploading recording:', error);
+    }
+  };
+
+  const generateVideoname = () => {
+    // Example: Generate a name based on current timestamp
+    const timestamp = new Date().getTime();
+    return `video_${timestamp}.webm`;
+  };
+
   return (
     <div className='room-container'>
       <div className='video-screen'>
       <video ref={videoRef} autoPlay muted />
       <div className='controls'>
         <Button checktoggle={toggleRecording} isCameraOn={recording} icon={<IoRecording />} />
-        {recordedChunks.length > 0 && (
+        {recordedChunks.length > 0 && (<div className='control-buttons'>
             <Button checktoggle={downloadRecording} isCameraOn={recording} icon={<FaDownload />} />
+            <Button checktoggle={uploadRecording} isCameraOn={recording} icon={<FaUpload />} />
+            </div>
           )}
       </div>
       </div>
